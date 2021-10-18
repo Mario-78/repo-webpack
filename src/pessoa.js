e3e026e8de26a105a51c2ec5a58b83e6fbe2567c
Mario-78 <mariojsws1@gmail.com>
2021-10-18 20:14:38 -0300

    Padrões de exportação e importação NODE e ES

---
+++ src/pessoa.js
@@ -0,0 +1,6 @@
+export default class Pessoa {
+    // module.exports = class Pessoa {
+    comprimentar() {
+        return "Bom dia!"
+    }
+}
---
+++ src/principal.js
@@ -0,0 +1,6 @@
+import Pessoa from './pessoa'
+const moduloA = require('./modulos/moduloA')
+    // const Pessoa = require('./pessoa')
+
+const atendente = new Pessoa
+console.log(atendente.comprimentar())
